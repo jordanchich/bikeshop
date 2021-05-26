d383ea4dac745e84689e07dc9658a679e3b6cabd
jordan chich <jordanchich@MacBook-Pro-de-jordan.local>
2021-05-26 15:44:08 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var stripe = require('stripe')('sk_test_o9sWtAKD4rmPgv3xfgyNsPwX001kmUDFnO');
-// To install the session mecanism, we need to enter the cmd : npm install express-session --save
-// Then, we can verify that the process worked well in our file package.json (line 13)
+
 var dataBike = [
   {name: 'Model BIKO45', price: 679, url: '/images/bike-1.jpg'},
   {name: 'Model ZOOK7', price: 799, url: '/images/bike-2.jpg'},
@@ -12,7 +11,7 @@ var dataBike = [
   {name: 'Model AMIG39', price: 599, url: '/images/bike-6.jpg'}
 ];
 
-// var dataCardBike = [];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -77,13 +76,11 @@ router.post('/update-shop', function(req, res, next) {
 
 
 
-// // // // // //
-// ***BONUS*** //
-// // // // // //
 
 
-// /* ***BONUS*** GET shop page. */
-// // This bonus aims to give the user the possibity to reach the shop page without clicking on buy
+
+
+// // give the user the possibity to reach the shop page without clicking on buy
 router.get('/shop', function(req, res, next) {
   if(req.session.dataCardBike == undefined) {
     req.session.dataCardBike = [];
@@ -93,46 +90,6 @@ router.get('/shop', function(req, res, next) {
 
 
 
-// /* ***BONUS*** POST update shop page. */
-// // This bonus aims to check to check if the requested quantity is 0, then delete the item, otherwise if not 0, then update the quantity
-// router.post('/update-shop', function(req, res, next) {
-//
-//   if (req.body.quantity == 0) {
-//     req.session.dataCardBike.splice(req.body.position, 1);
-//   } else {
-//     req.session.dataCardBike[req.body.position].quantity = req.body.quantity;
-//   }
-//
-//   res.render('shop', {dataCardBike: req.session.dataCardBike});
-// });
 
-
-
-
-// /* ***BONUS*** POST shop page. */
-// // // This bonus aims to check if the requested bike to add already exists in the shop basket, then we want to update the quantity of this bike but we do not want to add it, if not, add it.
-// router.post('/shop', function(req, res, next) {
-//
-//
-//   var mustbeUpdated = false;
-//   for (var i = 0; i < req.session.dataCardBike.length; i++) {
-//     if (req.body.bikeNameFromFront == req.session.dataCardBike[i].name) {
-//       mustbeUpdated = true;
-//       req.session.dataCardBike[i].quantity++
-//     }
-//   }
-//   if (mustbeUpdated == false) {
-//     req.session.dataCardBike.push(
-//       {
-//         name: req.body.bikeNameFromFront,
-//         price: req.body.bikePriceFromFront,
-//         url: req.body.bikeImageFromFront,
-//         quantity: req.body.bikeQuantityFromFront
-//       }
-//     );
-//   }
-//
-//   res.render('shop', {dataCardBike: req.session.dataCardBike});
-// });
 
 module.exports = router;
